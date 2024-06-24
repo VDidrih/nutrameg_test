9f8169ea6aa21fdbfb2ac3eff59224d94fdf54cc
Voldemars Didrihsons <voldo@Voldemarss-MacBook-Pro-2.local>
2024-06-25 02:18:17 +0300

    fix div hight

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <main className="relative overflow-scroll w-full">
       <Navbar />
-      <div className="md:h-screen w-full bg-[#E2E9E5] text-[#444134]">
+      <div className="h-max md:h-screen w-full bg-[#E2E9E5] text-[#444134]">
         <div className="h-max md:h-screen flex flex-col justify-evenly items-center md:flex-row">
           <GoalGrid goals={weightLossGoals} />
           <ResponsivBackground />
